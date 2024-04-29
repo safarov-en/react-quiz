bc68deaf25d3e2d1f6bfefd061745553b18c752f
safarov-en <safarov.elkhan110@gmail.com>
2024-04-29 17:22:28 +0500

    Menu button

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import MenuToggle from "../../components/Navigation/Menu-toggle/MenuToggle";
 import classes from './Layout.module.css';
 
 class Layout extends Component {
+  state = {
+    menu: false
+  }
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
   render() {
     return (
       <div className={classes.Layout}>
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>{this.props.children}</main>
       </div>
     );
