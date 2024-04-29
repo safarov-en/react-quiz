791ca8a4b06ec60da902c6dc8f4377ddea8f936e
safarov-en <safarov.elkhan110@gmail.com>
2024-04-29 15:39:14 +0500

    Choosing an answer

--- src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
@@ -1,9 +1,13 @@
 import classes from './AnswersItem.module.css';
 
 const AnswerItem = props => {
+    const cls = [classes.AnswerItem];
+    if (props.state) {
+        cls.push(classes[props.state]);
+    }
     return (
         <li
-            className={classes.AnswerItem}
+            className={cls.join(' ')}
             onClick={() => props.onAnswerClick(props.answer.id)}
         >
             {props.answer.text}
