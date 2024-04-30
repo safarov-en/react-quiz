009e8f4e0183c5fd6f7bc3f024dd212703ed3025
safarov-en <safarov.elkhan110@gmail.com>
2024-04-30 12:22:57 +0500

    Checking the form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import is from 'is_js';
 
 export default class Auth extends Component {
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -66,8 +67,12 @@ export default class Auth extends Component {
         control.touched = true;
         control.valid = this.validateControl(control.value, control.validation)
         formControls[controlName] = control
+        let isFormValid = true;
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid;
+        })
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     }
     renderInputs () {
@@ -98,12 +103,14 @@ export default class Auth extends Component {
                         <Button
                             type="success"
                             onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Войти
                         </Button>
                         <Button
                             type="primary"
                             onClick={this.registerHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Зарегистрироваться
                         </Button>
