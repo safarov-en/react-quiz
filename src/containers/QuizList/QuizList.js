4b7208633876b525ae81136458070f55084e16e2
safarov-en <safarov.elkhan110@gmail.com>
2024-05-01 14:47:22 +0500

    Download component

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import classes from "./QuizList.module.css";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import Loader from "../../components/UI/Loader/Loader";
 
 export default class QuizList extends Component {
   state = {
-    quizes: []
+    quizes: [],
+    loading: true
   }
   renderQuizes() {
     return this.state.quizes.map(quiz => {
@@ -27,7 +29,7 @@ export default class QuizList extends Component {
         })
       })
       this.setState({
-        quizes
+        quizes, loading: false
       })
     } catch (e) {
       console.log(e);
@@ -38,7 +40,11 @@ export default class QuizList extends Component {
       <div className={classes.QuizList}>
         <div>
           <h1>Список тестов</h1>
-          <ul>{this.renderQuizes()}</ul>
+          {
+            this.state.loading
+              ? <Loader />
+              : <ul>{this.renderQuizes()}</ul>
+          }
         </div>
       </div>
     );
