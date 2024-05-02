501a0e39fb0fd6066fd79ab42f956d741993ba76
safarov-en <safarov.elkhan110@gmail.com>
2024-05-02 18:02:06 +0500

    Test output in redux

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,4 +1,4 @@
-import { FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZ_SUCCESS } from "../actions/actionTypes"
+import { FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZ_SUCCESS, QUIZ_SET_STATE, FINISH_QUIZ, QUIZ_NEXT_QUESTION, QUIZ_RETRY } from "../actions/actionTypes"
 
 const initialState = {
     quizes: [],
@@ -35,6 +35,31 @@ export default function quizReducers(state = initialState, action) {
                 ...state,
                 loading: false,
                 quiz: action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state,
+                answerState: action.answerState,
+                results: action.results
+            }
+        case FINISH_QUIZ:
+            return {
+                ...state,
+                isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state,
+                answerState: null,
+                actionQuestion: action.number
+            }
+        case QUIZ_RETRY:
+            return {
+                ...state,
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             }
         default:
             return state
