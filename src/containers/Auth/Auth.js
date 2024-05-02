9007b70e321539d3e0a56d2e1c21c5293d0a2b8e
safarov-en <safarov.elkhan110@gmail.com>
2024-05-02 20:08:37 +0500

    Registration of redux

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.css';
-import axios from 'axios';
 import is from 'is_js';
+import { connect } from 'react-redux';
+import { auth } from '../../store/actions/auth';
 
-export default class Auth extends Component {
+class Auth extends Component {
     state = {
         isFormValid: false,
         formControls: {
@@ -35,31 +36,19 @@ export default class Auth extends Component {
             }
         }
     }
-    loginHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const pesponse = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAY9GewasmsoGk2Xrf-kg436qU0x0IjW5U', authData);
-            console.log(pesponse.data)
-        } catch (e) {
-            console.log(e);
-        }
+    loginHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true
+        )
     }
-    registerHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const pesponse = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAY9GewasmsoGk2Xrf-kg436qU0x0IjW5U', authData);
-            console.log(pesponse.data)
-        } catch (e) {
-            console.log(e);
-        }
+    registerHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        )
     }
     submitHandler = event => {
         event.preventDefault();
@@ -141,3 +130,11 @@ export default class Auth extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { AUTH_SUCCESS, AUTH_LOGOUT } from "./actionTypes";
+
+export function auth(email, password, isLogin) {
+    return async dispatch => {
+        const authData = {
+            email, password, returnSecureToken: true
+        }
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAY9GewasmsoGk2Xrf-kg436qU0x0IjW5U';
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAY9GewasmsoGk2Xrf-kg436qU0x0IjW5U';
+        }
+        const pesponse = await axios.post(url, authData);
+        const data = pesponse.data;
+        const expirationDate = new Date(new Date().getItem() + data.expriseIn * 1000)
+        localStorage.setItem('token', data.idToken);
+        localStorage.setItem('userId', data.localId);
+        localStorage.setItem('expirationDate', expirationDate);
+        dispatch(authSuccess(data.idToken))
+        dispatch(autoLogout(data.expriseIn))
+    }
+}
+
+export function autoLogout(time) {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, time * 1000)
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expirationDate');
+    return {
+        type: AUTH_LOGOUT
+    }
+}
+
+export function authSuccess(token) {
+    return {
+        type: AUTH_SUCCESS,
+        token
+    }
+}
